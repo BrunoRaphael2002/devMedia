4158f884a8aaa78ccaf82dc2b9cae06dec41831c
BrunoRaphael2002 <105222060+BrunoRaphael2002@users.noreply.github.com>
2023-10-04 09:34:46 -0300

    estudando react

---
+++ react/projetos-react/exercico-01/primeiro-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Header from './componentes/header';
+import Footer from './componentes/footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
