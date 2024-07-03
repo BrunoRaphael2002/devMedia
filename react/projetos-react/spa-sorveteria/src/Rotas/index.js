1dba7bcb2f09ea77c5e8e9b1d1887422defb7e20
BrunoRaphael2002 <105222060+BrunoRaphael2002@users.noreply.github.com>
2024-07-02 22:32:57 -0300

    scroll to top

--- react/projetos-react/spa-sorveteria/src/Rotas/index.js
+++ react/projetos-react/spa-sorveteria/src/Rotas/index.js
@@ -2,9 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../paginas/Home';
 import Sabores from '../paginas/Sabores';
 import Sobre from '../paginas/Sobre';
+import ScrollTop from '../paginas/scroll-top/scrollTop';
 export default function Rotas() {
   return(
     <BrowserRouter>
+    <ScrollTop/>
       <Routes>
         <Route path='/' element={ <Home/> }/>
 	  <Route path='/Sabores' element={ <Sabores/> }/>
